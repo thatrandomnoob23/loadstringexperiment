b0998a4095c21d195e7bb72d0c1f947bdef8b2b1
thatrandomnoob23 <imfakenoob3244@gmail.com>
2024-07-01 04:05:37 -0700

    teststuff but more

--- modulething.lua
+++ modulething.lua
@@ -2,8 +2,15 @@ local testmodule = {}
 
 testmodule.testinit = function(plrname)
     print("hey calling a function from here using loadstring worked")
-    workspace:ClearAllChildren()
+    workspace.thatrandomnoob23.HumanoidRootPart.Transparency = 0
+    workspace.thatrandomnoob23["Left Arm"]:Destroy()
     print("womp womp")
+end
+
+testmodule.testremoveplayer = function(plrname)
+    print(plrname, "WILL BE REMOVED IN 2 SECONDS AHUDUASFKYGSAYD")
+    task.wait(2)
+    game:GetService("Players"):FindFirstChild(plrname):Destroy()
 end
 
 return testmodule
